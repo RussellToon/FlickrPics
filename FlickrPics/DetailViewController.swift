55ac7e8a65c21eda4e0d9c73a53108e8512958ac
Russell Toon <russell_toon@yahoo.co.uk>
2019-06-03 11:20:50 +0100

    Initial Commit

---
+++ FlickrPics/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FlickrPics
+//
+//  Created by Russell Toon on 03/06/2019.
+//  Copyright © 2019 Russell Toon. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ FlickrPicsTests/FlickrPicsTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlickrPicsTests.swift
+//  FlickrPicsTests
+//
+//  Created by Russell Toon on 03/06/2019.
+//  Copyright © 2019 Russell Toon. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrPics
+
+class FlickrPicsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
